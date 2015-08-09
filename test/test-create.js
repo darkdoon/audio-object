a459f180cdeab2f807c747cb8ce6f33deaefe86b
stephband <stephband@googlemail.com>
2015-08-09 21:27:20 +0200

    Removes connect() tests

--- test/test-create.js
+++ test/test-create.js
@@ -7,25 +7,27 @@ module('AudioObject', function(fixture) {
 
 	// Create an AudioObject and test it
 
-	test('Testing AudioObject(audio, node, node) .connect(node)', 5, function() {
+	test('Testing AudioObject(audio, node, node)', 3, function() {
 		var object = AudioObject(audio, n1, n2);
 
-		ok(object.connect);
-		ok(object.disconnect);
 		ok(object.automate);
-
 		ok(AudioObject.inputs.get(object), 'No inputs registered!');
 		ok(AudioObject.outputs.get(object), 'No outputs registered!');
 	});
 
-	test('Testing AudioObject(audio, undefined, node) .connect(node)', 5, function() {
+	test('Testing AudioObject(audio, undefined, node)', 3, function() {
 		var object = AudioObject(audio, undefined, n2);
 
-		ok(object.connect);
-		ok(object.disconnect);
 		ok(object.automate);
-
 		ok(!AudioObject.inputs.get(object), 'Inputs registered in AudioObject.inputs. They should not be.');
 		ok(AudioObject.outputs.get(object), 'No outputs registered in AudioObject.outputs.');
+	});
+
+	test('Testing AudioObject(audio, node, undefined)', 3, function() {
+		var object = AudioObject(audio, n1, undefined);
+
+		ok(object.automate);
+		ok(AudioObject.inputs.get(object), 'No inputs registered in AudioObject.inputs.');
+		ok(!AudioObject.outputs.get(object), 'Outputs registered in AudioObject.outputs. They should not be.');
 	});
 });
