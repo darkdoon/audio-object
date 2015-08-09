9a99522069780efb44cdface61d26e31949d9a8f
stephband <stephband@googlemail.com>
2015-08-09 21:30:35 +0200

    Tests .getInput and .getoutput

--- test/test-create.js
+++ test/test-create.js
@@ -11,23 +11,23 @@ module('AudioObject', function(fixture) {
 		var object = AudioObject(audio, n1, n2);
 
 		ok(object.automate);
-		ok(AudioObject.inputs.get(object), 'No inputs registered!');
-		ok(AudioObject.outputs.get(object), 'No outputs registered!');
+		ok(AudioObject.getInput(object), 'No inputs registered!');
+		ok(AudioObject.getOutput(object), 'No outputs registered!');
 	});
 
 	test('Testing AudioObject(audio, undefined, node)', 3, function() {
 		var object = AudioObject(audio, undefined, n2);
 
 		ok(object.automate);
-		ok(!AudioObject.inputs.get(object), 'Inputs registered in AudioObject.inputs. They should not be.');
-		ok(AudioObject.outputs.get(object), 'No outputs registered in AudioObject.outputs.');
+		ok(!AudioObject.getInput(object), 'Inputs registered in AudioObject.inputs. They should not be.');
+		ok(AudioObject.getOutput(object), 'No outputs registered in AudioObject.outputs.');
 	});
 
 	test('Testing AudioObject(audio, node, undefined)', 3, function() {
 		var object = AudioObject(audio, n1, undefined);
 
 		ok(object.automate);
-		ok(AudioObject.inputs.get(object), 'No inputs registered in AudioObject.inputs.');
-		ok(!AudioObject.outputs.get(object), 'Outputs registered in AudioObject.outputs. They should not be.');
+		ok(AudioObject.getInput(object), 'No inputs registered in AudioObject.inputs.');
+		ok(!AudioObject.getOutput(object), 'Outputs registered in AudioObject.outputs. They should not be.');
 	});
 });
